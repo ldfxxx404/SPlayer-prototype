e2ffce03d827a0d458a6d212541aab31b84d0b66
lnssr <gifzoner5@gmail.com>
2024-06-18 11:05:39 +0300

    Revert "Merge pull request #4 from ldfxxx404/develop"
    This reverts commit 62130bb0938c7751788e4f03ae65e4ba5dbe8086, reversing
    changes made to 6c3b27ae368b241ba73f9e826bfe210fa19ca750.

--- src/output/CLI_menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLI_MENU_H
-#define CLI_MENU_H
-
-#include <string>
-
-std::string browseFile(const std::string &directory);
-
-#endif // CLI_MENU_H
--- src/output/control.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONTROL_H
-#define CONTROL_H
-
-void setNonCanonicalMode(bool enable);
-
-#endif // CONTROL_H
