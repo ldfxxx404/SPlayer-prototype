ad574e730595fa54136bbff53eb80db0a69a9fe0
ldfxxx404 <limur9990@gmail.com>
2024-06-18 23:55:54 +0300

    Minor fixes, also added directory navigation with the ability to select the file to be played.

--- src/CLI_menu.h
+++ src/CLI_menu.h
@@ -4,99 +4,142 @@
 #include <iostream>
 #include <ncurses.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-std::vector<std::string> getFiles(const std::string &path){
+const std::string mainDirectory = "/"; /* Base directory */
 
+std::vector<std::string> getFiles(const std::string &path) /* list the files in the directory. Start */
+{
     std::vector<std::string> files;
     DIR *currentDir;
     struct dirent *entry;
-    
-    if ((currentDir = opendir(path.c_str())) != NULL){
 
-        while ((entry = readdir(currentDir)) != NULL){
+    std::string fullPath = mainDirectory + "/" + path;
 
-            if (entry -> d_type == DT_REG){
+    if ((currentDir = opendir(fullPath.c_str())) != NULL)
+    {
+        files.push_back("...");
 
-               files.push_back(entry -> d_name);
-            }
-            
+        while ((entry = readdir(currentDir)) != NULL)
+        {
+            std::string name = entry->d_name;
+            if (name == "." || name == ".." || name[0] == '.')
+                continue;
+
+            files.push_back(name);
         }
         closedir(currentDir);
     }
-    else{
-        
+    else
+    {
         perror("Failed to open directory");
     }
 
     return files;
-}
+} /* End */
 
-std::string browseFile(const std::string &path){
-
+std::string browseFile(const std::string &initPath) /* Browse files in current dir. Start */
+{
     initscr();
     noecho();
     cbreak();
     keypad(stdscr, TRUE);
 
-   std::vector<std::string> files = getFiles(path);
+    std::string currentPath = initPath;
+    std::vector<std::string> files = getFiles(currentPath);
 
-   if (files.empty()){
-
-    endwin();
-    return "";
-   }
-   
+    if (files.empty())
+    {
+        endwin();
+        return "";
+    }
 
     int highlight = 0;
     int choice;
     int n_choices = files.size();
 
-    while (1){
-
+    while (true)
+    {
         clear();
+        mvprintw(0, 0, "Current dir: %s", currentPath.c_str());
 
-        for (int i = 0; i < n_choices; ++i){
-
-            if (i == highlight){
-
+        for (int i = 0; i < n_choices; ++i)
+        {
+            if (i == highlight)
+            {
                 attron(A_REVERSE);
-                mvprintw(i, 0, "%s", files[i].c_str());
+                mvprintw(i + 1, 0, "%s", files[i].c_str());
                 attroff(A_REVERSE);
             }
-
-            else{
-
-                mvprintw(i, 0, "%s", files[i].c_str());   
+            else
+            {
+                mvprintw(i + 1, 0, "%s", files[i].c_str());
             }
         }
-    
-      choice = getch();
 
-        switch (choice){
+        choice = getch();
+        std::string selected = files[highlight];
 
-            case KEY_UP:
-                if (highlight > 0){
+        switch (choice)
+        {
+        case KEY_UP:
+            if (highlight > 0)
+            {
+                --highlight;
+            }
+            break;
 
-                    --highlight;
+        case KEY_DOWN:
+            if (highlight < n_choices - 1)
+            {
+                ++highlight;
+            }
+            break;
+
+        case 10: // Enter key
+            if (selected == "...")
+            {
+                size_t position = currentPath.find_last_of("/");
+                if (position != std::string::npos)
+                {
+                    currentPath = currentPath.substr(0, position);
+                    if (currentPath.empty()) {
+                        currentPath = "/";
+                    }
+                    files = getFiles(currentPath);
+                    highlight = 0;
+                    n_choices = files.size();
                 }
-                break;
-
-            case KEY_DOWN:
-                if (highlight < n_choices - 1){
-                    
-                    ++highlight;
+            }
+            else
+            {
+                std::string fullPath = currentPath + "/" + selected;
+                struct stat statbuf;
+                if (stat(fullPath.c_str(), &statbuf) == 0)
+                {
+                    if (S_ISREG(statbuf.st_mode))
+                    {
+                        endwin();
+                        return fullPath;
+                    }
+                    else if (S_ISDIR(statbuf.st_mode))
+                    {
+                        currentPath = fullPath;
+                        files = getFiles(currentPath);
+                        highlight = 0;
+                        n_choices = files.size();
+                    }
                 }
-                break;
-            
-            case 10:
-                endwin();
-                return files[highlight];
-            
-            default:
-                break;
+            }
+            break;
+
+        default:
+            break;
         }
     }
 
     endwin();
     return "";
 }
+    /*End*/
--- src/control.h
+++ src/control.h
@@ -3,18 +3,21 @@
 #include <termios.h>
 #include <unistd.h>
 
-void setNonCanonicalMode(bool enable){
+void setNonCanonicalMode(bool enable)
+{
 
     struct termios ttystate;
     tcgetattr(STDIN_FILENO, &ttystate);
 
-    if (enable){
+    if (enable)
+    {
 
         ttystate.c_lflag &= ~(ICANON | ECHO);
         ttystate.c_cc[VMIN] = 1;
     }
-    else{
-        
+    else
+    {
+
         ttystate.c_lflag |= ICANON;
     }
 
